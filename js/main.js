09326ce66af243947c9f66c8acff12caf05f6331
Ikramovdev <mubinullakh@gmail.com>
2024-07-28 21:28:11 +0500

    intial commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var questions1 = "HTML nima ?"
+var answer1 = prompt(questions1)
+console.log(questions1,answer1);
+
+var questions2 = "HTML va CSS o'rtasidagi farq nima?"
+var answer2 = prompt(questions2)
+console.log(questions2,answer2);
+
+var questions3 = "html ning boshlang'ich codi nma ded ataladi ?"
+var answer3 = prompt(questions3)
+console.log(questions3,answer3);
+
+var questions4 = "Githubning bizning ishimizga orni qanday ?"
+var answer4 = prompt(questions4)
+console.log(questions4,answer4);    
+
+var questions5 = "Site codini yozayotganimizda nega container beramiz ?"
+var answer5 = prompt(questions5)
+console.log(questions5,answer5);
+
+var questions6 = "Yozuvni Bold holatiga kelishtrishimiz uchun qanday amal bajaramiz ?"
+var answer6 = prompt(questions6)
+console.log(questions6,answer6);
+
+var questions7 = "Responsive nma va u qanday vazifani bajaradi  ?"
+var answer7 = prompt(questions7)
+console.log(questions7,answer7);
+
+var questions8 = "margin va paddingning farqi nmada ?"
+var answer8 = prompt(questions8)
+console.log(questions8,answer8);
+
+var questions9 = "h1,h2,h3,h4,h5,h6, bular siteda qanday vaifani bajaradi ?"
+var answer9 = prompt(questions9)
+console.log(questions9,answer9);
+
+var questions10 = "html taglarni sanab bering ?"
+var answer10 = prompt(questions10)
+console.log(questions10,answer10);
+
+var questions11 = "Moslashuvchan dizaynlarni sanab bering ? "
+var answer11 = prompt(questions11)
+console.log(questions11,answer11);
+
+var questions12 = "Tailwind, css ,sas bular  nma ?"
+var answer12 = prompt(questions12)
+console.log(questions12,answer12);
+
+var questions13 = "hr nma vazifani bajaradi ?"
+var answer13 = prompt(questions13)
+console.log(questions13,answer13);
+
+
+var questions14 = "liner gradient nega faqat backgroundni qabul qiladi ?"
+var answer14 = prompt(questions14)
+console.log(questions14,answer14);
+
+var questions15 = "yozuvlarni o'rtaga yani centerga olib kelish uchun qanday amal bajariladi ?"
+var answer15 = prompt(questions15)
+console.log(questions15,answer15);
+
+alert("Savol javob tugadi")
